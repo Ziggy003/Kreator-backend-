aedec35505d07e22fb8bceba898180522a8043e5
Samuel Zinga <szinga007@gmail.com>
2026-04-06 04:43:42 +0100

    Criar oauth.js

---
+++ oauth.js
@@ -0,0 +1,12 @@
+module.exports = {
+  google: {
+    clientID: "SEU_GOOGLE_CLIENT_ID",
+    clientSecret: "SEU_GOOGLE_CLIENT_SECRET",
+    callbackURL: "https://kreator-backend-jyqq.onrender.com/auth/google/callback"
+  },
+  facebook: {
+    clientID: "SEU_FACEBOOK_APP_ID",
+    clientSecret: "SEU_FACEBOOK_APP_SECRET",
+    callbackURL: "https://kreator-backend-jyqq.onrender.com/auth/facebook/callback"
+  }
+};
